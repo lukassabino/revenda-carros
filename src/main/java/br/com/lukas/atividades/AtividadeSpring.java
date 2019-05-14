32afaad96439ed52074d17a46fd1fb5ea769d648
lukassabinoo <lukassabino_@hotmail.com>
2019-05-14 13:33:00 -0300

    Ajuste do nome da classe do Spring

--- src/main/java/br/com/lukas/atividades/AtividadeSpring.java
+++ src/main/java/br/com/lukas/atividades/AtividadeSpring.java
@@ -1,4 +1,4 @@
-package br.com.lukas.atividades_spring;
+package br.com.lukas.atividades;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
